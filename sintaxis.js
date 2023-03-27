4856f7c22104e0fdfa1a1a1a0e10b2c883979e65
jorgelinasapp <126989877+jorgelinasapp@users.noreply.github.com>
2023-03-26 22:10:45 -0300

    Add files via upload

--- sintaxis.js
+++ sintaxis.js
@@ -1,30 +1,7 @@
+window.addEventListener("load", function( ) {
+    document.getElementById("CambiarColor").addEventListener("click", function () {
+        var ColorFondo = document. getElementById ("ColorFondo").value;
+        document.getElementById("cuerpo").setAttribute("bgcolor", ColorFondo);
 
-function cambiarColor(){
-    document.body.style.backgroundColor="red" 
-    /* document.getElementById('boton_color').addEventListener('click', function(){debugger
-       /*  document.body.style.backgroundColor= 'red';  
-        document.body.style.backgroundColor="red" 
-    }); */
-}
-
-const botonColor = document.getElementById('boton_color');
-botonColor.addEventListener("click",function(){
-    document.body.style.backgroundColor="white"
+    })
 })
-
-const span = document.getElementById('algo');
-span.addEventListener("click",function(){
-    document.body.style.color="blue" 
-
-})
-
-window.addEventListener("load", function() {document.getElementById ("CabiarColor"). addEventListener ("click", function () {
-    var ColorFondo = document.getElementById ("ColorFondo").value;
-
-    document.getElementById("cuerpo").setAttribute(backgroundColor,ColorFondo);
-
-})
-
-}
-
-)
